36c28c0f389d82c421a7ebf77f36aa12e06b938f
taekim90 <taehk15@gmail.com>
2022-02-27 16:15:07 -0800

    installation of packages

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const app = express() 
+const ejsLayouts = require('express-ejs-layouts') 
+require('dotenv').config()
+// const axios = require('axios')
+// const cookieParser = require('cookie-parser')
+// const cryptoJS = require('crypto-js')
+// const db = require('./models/index.js')
+
+'use strict';
+const yelp = require('yelp-fusion');
+const apiKey = process.env.YELP_API_KEY
+
+
+
+// MIDDLEWARE
+app.set('view engine', 'ejs')
+app.use(ejsLayouts)
+
+// CONTROLLERS
+
+// ROUTES
+app.get('/', (req,res) => {
+    const searchRequest = {
+        term: 'things to do',
+        location: 'portland',
+    };
+      
+    const client = yelp.client(apiKey);
+      
+    client.search(searchRequest)
+    .then((response) => {
+        console.log(response.jsonBody);
+        res.json(response.jsonBody)
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
+})
+
+
+
+const PORT = process.env.PORT || 8000
+app.listen(PORT, () => {
+    console.log(`Running on ${PORT}`)
+})
