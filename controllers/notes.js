fcf27abcb3e873474f43d12a31c85658fbd57f72
taekim90 <taehk15@gmail.com>
2022-03-01 19:40:18 -0800

    creation of notes get route

--- controllers/notes.js
+++ controllers/notes.js
@@ -7,5 +7,8 @@ require('dotenv').config()
 const yelp = require('yelp-fusion');
 const apiKey = process.env.YELP_API_KEY
 
+router.get('/', async (req, res) => {
+    res.render("notes/notes.ejs")
+})
 
 module.exports = router
--- controllers/places.js
+++ controllers/places.js
@@ -2,14 +2,25 @@ const express = require('express')
 const router = express.Router()
 const db = require('../models')
 require('dotenv').config()
+const methodOverride = require('method-override')
 
 'use strict';
 const yelp = require('yelp-fusion');
 const user = require('../models/user');
 const apiKey = process.env.YELP_API_KEY
 
+router.use(methodOverride('_method'));
+
 router.get('/', async (req, res) => {
-    res.render('places/places.ejs')
+    try {
+        const savedPlaces = await db.place.findAll({}) // findAll gives us an array
+        const savedNotes = await db.note.findAll({})
+        console.log(savedPlaces)
+        res.render('places/places.ejs', {placesArray: savedPlaces})
+    } catch (error) {
+        console.log(error)
+    }
+    // res.render('places/places.ejs')
 })
 
 router.post('/', async (req, res) => {
@@ -21,19 +32,19 @@ router.post('/', async (req, res) => {
                 yelpUrl: req.body.yelpUrl,
                 category: req.body.category,
             }
-        })
+        })   
+        console.log("Place :", place.id)
+        console.log(place)
+        console.log("User :", res.locals.currentUser.dataValues.id)
 
-    console.log("Place :", place.id)
-    console.log("User :", res.locals.currentUser.dataValues.id)
-
-    console.log(place)
-    const user = await db.user.findOne({
-        where: {
-            id: res.locals.currentUser.dataValues.id
+        const user = await db.user.findOne({
+            where: {
+                id: res.locals.currentUser.dataValues.id
         }
     })
     console.log(user)
     await place.addUser(user)
+    // res.render('places/places.ejs')
     } catch (error) {
         console.log(error)
     }
